e49d23e70952901f8e6bf380af49289fe6f5cc90
Dakotah Ducharme <dakotahbducharme@gmail.com>
2019-11-08 13:27:58 -0800

    I was working in the wrong file, but added the proper code now

--- sketch.js
+++ sketch.js
@@ -1,12 +1,48 @@
+let snowflakes = [];
+
 function setup() {
   createCanvas(800, 1200);
+  fill(240);
+  noStroke();
 }
 
 function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(225, 200, 200);
+  background('blue');
+  let t = frameCount / 60;
+
+  for (let i = 0; i < random(5); i++) {
+    snowflakes.push(new snowflake());
   }
-  rect(mouseX, mouseY, 80, 80);
+
+  for (let flake of snowflakes) {
+    flake.update(t);
+    flake.display();
+  }
+}
+
+function snowflake() {
+  this.posX = 0;
+  this.posY = random(-50, 0);
+  this.initialAngle = random(0, 2 * PI);
+  this.size = random(2, 5);
+
+  this.radius = sqrt(random(pow(width / 2, 2)));
+
+  this.update = function (time) {
+    let w = 0.6;
+    let angle = w * time + this.initialAngle;
+    this.posX = width / 2 + this.radius * sin(angle);
+
+    this.posY += pow(this.size, 0.5); //diff size snowflakes fall at slightly diff y speeds
+
+    //delete sf if it passes edge of screen
+    if (this.posY > height) {
+      let index = snowflakes.indexOf(this);
+      snowflakes.splice(index, 1);
+    }
+  }
+
+  this.display = function () {
+    ellipse(this.posX, this.posY, this.size);
+  };
 }
